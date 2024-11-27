7735d2996e6a727a8f2bcec06b1d02285bd8e690
Danilo Teixeira <danilo@lendwell.ie>
2024-11-27 08:56:28 -0300

    add meta tag to test IOS

--- src/app/pages/lock-screen/lock-screen.component.ts
+++ src/app/pages/lock-screen/lock-screen.component.ts
@@ -26,10 +26,6 @@ export class LockScreenComponent implements OnDestroy {
     }
   }
 
-  ngOnInit(): void {
-    // this.openFullscreen();
-  }
-
   ngOnDestroy(): void {
     // this.closeFullscreen();
     screenfull.exit();
@@ -44,43 +40,43 @@ export class LockScreenComponent implements OnDestroy {
     (window.screen.orientation as any)?.unlock();
   }
 
-  private openFullscreen() {
-    const el = this.document?.documentElement;
+  // private openFullscreen() {
+  //   const el = this.document?.documentElement;
 
-    if (el?.requestFullscreen) {
-      el?.requestFullscreen();
-    } else if (el?.mozRequestFullScreen) {
-      /* Firefox */
-      el?.mozRequestFullScreen();
-    } else if (el?.webkitRequestFullscreen) {
-      /* Chrome, Safari and Opera */
-      el?.webkitRequestFullscreen();
-    } else if (el?.msRequestFullscreen) {
-      /* IE/Edge */
-      el?.msRequestFullscreen();
-    }
+  //   if (el?.requestFullscreen) {
+  //     el?.requestFullscreen();
+  //   } else if (el?.mozRequestFullScreen) {
+  //     /* Firefox */
+  //     el?.mozRequestFullScreen();
+  //   } else if (el?.webkitRequestFullscreen) {
+  //     /* Chrome, Safari and Opera */
+  //     el?.webkitRequestFullscreen();
+  //   } else if (el?.msRequestFullscreen) {
+  //     /* IE/Edge */
+  //     el?.msRequestFullscreen();
+  //   }
 
-    if (this.platform.ANDROID || this.platform.IOS) {
-      this.lockOrientation();
-    }
-  }
-  /* Close fullscreen */
-  private closeFullscreen() {
-    if (this.document?.exitFullscreen) {
-      this.document?.exitFullscreen();
-    } else if (this.document?.mozCancelFullScreen) {
-      /* Firefox */
-      this.document?.mozCancelFullScreen();
-    } else if (this.document?.webkitExitFullscreen) {
-      /* Chrome, Safari and Opera */
-      this.document?.webkitExitFullscreen();
-    } else if (this.document?.msExitFullscreen) {
-      /* IE/Edge */
-      this.document?.msExitFullscreen();
-    }
+  //   if (this.platform.ANDROID || this.platform.IOS) {
+  //     this.lockOrientation();
+  //   }
+  // }
+  // /* Close fullscreen */
+  // private closeFullscreen() {
+  //   if (this.document?.exitFullscreen) {
+  //     this.document?.exitFullscreen();
+  //   } else if (this.document?.mozCancelFullScreen) {
+  //     /* Firefox */
+  //     this.document?.mozCancelFullScreen();
+  //   } else if (this.document?.webkitExitFullscreen) {
+  //     /* Chrome, Safari and Opera */
+  //     this.document?.webkitExitFullscreen();
+  //   } else if (this.document?.msExitFullscreen) {
+  //     /* IE/Edge */
+  //     this.document?.msExitFullscreen();
+  //   }
 
-    if (this.platform.ANDROID || this.platform.IOS) {
-      this.unlockOrientation();
-    }
-  }
+  //   if (this.platform.ANDROID || this.platform.IOS) {
+  //     this.unlockOrientation();
+  //   }
+  // }
 }
