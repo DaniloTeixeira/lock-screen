fc88ce08cfa6b84fd887eabf2914f8451a65b20f
Danilo Teixeira <danilo@lendwell.ie>
2024-11-27 00:07:11 -0300

    Add close full screen

--- src/app/pages/lock-screen/lock-screen.component.ts
+++ src/app/pages/lock-screen/lock-screen.component.ts
@@ -15,6 +15,8 @@ import { RouterModule } from "@angular/router";
       <a routerLink="../">Back to App</a>
       <h1>Lock Screen works!</h1>
     </div>
+
+    <button (click)="openFullscreen()">click</button>
   `,
 })
 export class LockScreenComponent implements OnDestroy {
@@ -23,7 +25,6 @@ export class LockScreenComponent implements OnDestroy {
   private document = Inject(DOCUMENT);
 
   ngOnInit(): void {
-    this.el = this.document.documentElement;
     this.openFullscreen();
 
     if (this.platform.ANDROID || this.platform.IOS) {
@@ -35,33 +36,39 @@ export class LockScreenComponent implements OnDestroy {
     this.closeFullscreen();
   }
 
-  private openFullscreen(): void {
-    const requestFullscreen =
-      this.el.requestFullscreen ||
-      this.el.mozRequestFullScreen || // Para Firefox
-      this.el.webkitRequestFullscreen || // Para Chrome, Safari, Opera
-      this.el.msRequestFullscreen; // Para IE/Edge
-
-    if (requestFullscreen) {
-      requestFullscreen.call(this.el);
-    }
-
-    this.blockOrientation();
-  }
-
-  private closeFullscreen(): void {
-    const exitFullscreen =
-      this.el.exitFullscreen ||
-      this.el.mozCancelFullScreen || // Para Firefox
-      this.el.webkitExitFullscreen || // Para Chrome, Safari, Opera
-      this.el.msExitFullscreen; // Para IE/Edge
-
-    if (exitFullscreen) {
-      exitFullscreen.call(this.el);
-    }
-  }
-
   private blockOrientation() {
     (window.screen.orientation as any)?.lock("landscape");
+  }
+
+  openFullscreen() {
+    this.el = this.document?.documentElement;
+
+    if (this.el?.requestFullscreen) {
+      this.el?.requestFullscreen();
+    } else if (this.el?.mozRequestFullScreen) {
+      /* Firefox */
+      this.el?.mozRequestFullScreen();
+    } else if (this.el?.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.el?.webkitRequestFullscreen();
+    } else if (this.el?.msRequestFullscreen) {
+      /* IE/Edge */
+      this.el?.msRequestFullscreen();
+    }
+  }
+  /* Close fullscreen */
+  closeFullscreen() {
+    if (this.document?.exitFullscreen) {
+      this.document?.exitFullscreen();
+    } else if (this.document?.mozCancelFullScreen) {
+      /* Firefox */
+      this.document?.mozCancelFullScreen();
+    } else if (this.document?.webkitExitFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.document?.webkitExitFullscreen();
+    } else if (this.document?.msExitFullscreen) {
+      /* IE/Edge */
+      this.document?.msExitFullscreen();
+    }
   }
 }
